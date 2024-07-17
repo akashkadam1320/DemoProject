2b59432bf9bc7d66ac8d2e96764d13e660b8b558
akashkadam1320 <akashkadam1320@gmail.com>
2024-07-17 12:44:28 +0530

    polymorphism Example

---
+++ DemoProject/DynamicPolymorphism.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+
+{
+    // Base Class 
+    // Method NAme same Parmeter Aslo same but different class
+    class Shape
+    {
+        // Virtual method
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a shape");
+        }
+    }
+    // Derved class
+    class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Circle");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Rectangle");
+        }
+    }
+        internal class DynamicPolymorphism
+    {
+        static void Main()
+        {
+            // Creating Object
+            Shape shape1 = new Shape();
+            Shape shape2 = new Circle();
+            Shape shape3 = new Rectangle();
+
+            // Calling Method form each object
+            shape1.Draw();
+            shape2.Draw();
+            shape3.Draw();
+        }
+    }
+}
---
+++ DemoProject/StaticPolymorphism.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject
+{
+    class MathOpertion
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+    }
+    internal class StaticPolymorphism
+    {
+        static void Main()
+        {
+            MathOpertion math = new MathOpertion();
+            // call overloading method
+
+            int sum1 = math.Add(1, 2);
+            double sum2 = math.Add(2.5, 3);
+
+            Console.WriteLine("Sum Of Integers:"+sum1);
+            Console.WriteLine("sum Of Doubles:"+ sum2);
+        }
+    }
+}
